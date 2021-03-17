149ad31841f76511ed63d51d4c982cc7fe366e38
tagun9921 <tagun9921@hotmail.com>
2021-03-17 21:02:11 +0700

    re-commit

--- Inventory/nuxt.config.js
+++ Inventory/nuxt.config.js
@@ -1,9 +1,6 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
-  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
-
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - Inventory',
@@ -46,7 +43,7 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-     dark: true,
+      dark: true,
       themes: {
         dark: {
           primary: colors.blue.darken2,
@@ -56,16 +53,7 @@ export default {
           warning: colors.amber.base,
           error: colors.deepOrange.accent4,
           success: colors.green.accent3
-        },
-        light: {
-                    primary: colors.blue.darken3,
-                    secondary: colors.amber.darken3,
-                    accent: colors.grey.darken3,
-                    info: colors.teal.lighten1,
-                    warning: colors.amber.base,
-                    error: colors.deepOrange.accent4,
-                    success: colors.green.accent3
-                }
+        }
       }
     }
   },
