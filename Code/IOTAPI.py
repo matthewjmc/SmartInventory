d11672ed46f0308d2873f6c7f3531729fe48f596
matthewjmc <matthew@mcmullin.one>
2021-03-17 21:00:13 +0000

    API Update

--- Code/IOTAPI.py
+++ Code/IOTAPI.py
@@ -1,5 +1,5 @@
 import flask
-from flask import request,jsonify
+from flask import request,jsonify,redirect, url_for, Response
 from flask_mysqldb import MySQL
 import sys
 print(sys.version)
@@ -7,16 +7,72 @@ print(sys.version)
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
  
-app.config['MYSQL_HOST'] = 'iot.mcmullin.org'
+app.config['MYSQL_HOST'] = 'mariadb'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = '123456789'
-app.config['MYSQL_DB'] = 'MyDB'
+app.config['MYSQL_DB'] = 'Inventory'
+
+db = MySQL(app)
+
+@app.route('/api/v1/inventory', methods=["get"])
+def GetInventory():
+    header = dict(request.headers)
+    if header.get("Content-Type")=="application/json":
+        cursor = db.connection.cursor()
+        cursor.execute("SELECT item_name,description,availability FROM Items;")
+        result = cursor.fetchall()
+        if result:
+            return jsonify(result)
+    else:
+        result = {
+            "Error": "Invalid Request"
+        }
+        return jsonify(result),400
 
 
+@app.route('/api/v1/withdraw',methods=["get"])
+def GetWithdrawal():
+    header = dict(request.headers)
+    data = request.json
+    if header.get("Content-Type")=="application/json":
+        if data["command"] == "all":
+            ### Change SQL Statements
+            cursor = db.connection.cursor()
+            cursor.execute("SELECT item_name,description,availability FROM Items;")
+            result = cursor.fetchall()
+            return jsonify({"result":"Request ALL Data"})
+
+        elif data["command"] == "userID":
+             ### Change SQL Statements and Add check For values 
+            cursor = db.connection.cursor()
+            cursor.execute("SELECT item_name,description,availability FROM Items;")
+            result = cursor.fetchall()
+            return jsonify({"result":"Request userID Data"})
+        
+        elif data["command"] == "itemID":
+             ### Change SQL Statements and Add check for Values
+            cursor = db.connection.cursor()
+            cursor.execute("SELECT item_name,description,availability FROM Items;")
+            result = cursor.fetchall()
+            return jsonify({"result":"Request itemID Data"})
+        else:
+            result = {
+                "Error": "Invalid Request"
+            }
+            return jsonify(result),400
+    
+    else:
+        result = {
+                "Error": "Invalid Request"
+            }
+        return jsonify(result),400
+
+        
 
 
-@app.route('/', methods=['GET'])
-def home():
-    return "<h1>Wrong Page Man</h1><p>Only For API</p>"
+@app.errorhandler(404)
+def page_not_found(e):
+    return "<h1>404</h1><p>The resource could not be found.</p>", 404
+
 
 app.run(host="0.0.0.0",port=5000)
