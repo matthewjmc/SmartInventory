ab566e942770ca16042de9b2708690fe036b995f
tagun9921 <tagun9921@hotmail.com>
2021-03-17 22:58:06 +0700

    Cleaning unessential components.

---
+++ Inventory/pages/Login/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>
+        CIE Inventory System 
+        </h1>
+        <p>
+            Please login to access the system.
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        head(){
+            return{
+            title: 'Homepage',
+            meta: [
+                { hid: 'description',
+                name: 'description',   
+                content: 'Inventory System'
+                }
+            ]
+            }
+        }
+    }
+</script>
