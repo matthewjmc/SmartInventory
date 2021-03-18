ad185467dafea5e108b818067491392c93d70908
matthewjmc <matthew@mcmullin.one>
2021-03-18 09:30:30 +0000

    Update API

--- Code/APIDocumentation.md
+++ Code/APIDocumentation.md
@@ -7,21 +7,31 @@ GET /api/v1/inventory
 Content-Type: application/json
 ```
 ### Return
-```[
+```
+[
     [
         "HoloLens",
         "Microsoft Hololens",
-        1
+        1,
+        3
     ],
     [
         "Oculus VR",
         "Oculus Virtual Reality Goggles",
-        1
+        1,
+        4
     ],
     [
         "HTC VIVE",
         "HTC Vive Virtual Reality Goggles",
-        1
+        1,
+        3
+    ],
+    [
+        "NZXT Computers",
+        "NZXT High Performance Computers",
+        1,
+        5
     ]
 ]
 ```
@@ -32,7 +42,7 @@ Content-Type: application/json
 ```
 GET /api/v1/withdraw
 Content-Type: application/json
-Data: # Options {all, userID, ItemID}
+Data: # Options {all, userID, ItemID} Body not Header
 ```
 ### Data Format in Header
 ```
@@ -48,7 +58,7 @@ Data:{
 ```
 ```
 Data:{
-    command: all,
+    command: itemID,
     value: "7"
 }
 ```
--- Code/IOTAPI.py
+++ Code/IOTAPI.py
@@ -19,13 +19,23 @@ def GetInventory():
     header = dict(request.headers)
     if header.get("Content-Type")=="application/json":
         cursor = db.connection.cursor()
-        cursor.execute("SELECT item_name,description,availability FROM Items;")
+        cursor.execute("SELECT Items.item_name, Items.description, Stock.availability, Stock.amount FROM Stock INNER JOIN Items ON Stock.itemID=Items.itemID;")
         result = cursor.fetchall()
         if result:
-            return jsonify(result)
+            print(result,flush=True)
+            temp = []
+            for i in result:
+                total = {
+                    "name":i[0],
+                    "desc":i[1],
+                    "available":i[2],
+                    "quantity":i[3]
+                }
+                temp.append(total)
+            return jsonify(temp)
     else:
         result = {
-            "Error": "Invalid Request"
+            "Error": "Invalid Content-Type"
         }
         return jsonify(result),400
 
@@ -34,27 +44,60 @@ def GetInventory():
 def GetWithdrawal():
     header = dict(request.headers)
     data = request.json
+    command  = data["command"]
     if header.get("Content-Type")=="application/json":
-        if data["command"] == "all":
-            ### Change SQL Statements
+        if command == "all":
             cursor = db.connection.cursor()
-            cursor.execute("SELECT item_name,description,availability FROM Items;")
+            cursor.execute("SELECT Items.item_name,Users.userID,Users.firstname,Users.lastname,Borrow_Record.date_borrowed,Borrow_Record.expected_return_date FROM Borrow_Record INNER JOIN Users ON Borrow_Record.userID=Users.userID INNER JOIN Items ON Items.itemID=Borrow_Record.itemID;")
             result = cursor.fetchall()
-            return jsonify({"result":"Request ALL Data"})
+            temp = []
+            for i in result:
+                total = {
+                    "itemName":i[0],
+                    "userID":i[1],
+                    "firstname":i[2],
+                    "lastname":i[3],
+                    "dateBorrowed":i[4],
+                    "expectedReturn":i[5]
+                }
+                temp.append(total)
+            return jsonify(temp)
 
-        elif data["command"] == "userID":
-             ### Change SQL Statements and Add check For values 
+        elif command == "userID" and "value" in data:
+            values = data["value"]
             cursor = db.connection.cursor()
-            cursor.execute("SELECT item_name,description,availability FROM Items;")
+            cursor.execute(f"SELECT Items.item_name,Users.userID,Users.firstname,Users.lastname,Borrow_Record.date_borrowed,Borrow_Record.expected_return_date FROM Borrow_Record INNER JOIN Users ON Borrow_Record.userID=Users.userID INNER JOIN Items ON Items.itemID=Borrow_Record.itemID WHERE Users.userID= {values};")
             result = cursor.fetchall()
-            return jsonify({"result":"Request userID Data"})
-        
-        elif data["command"] == "itemID":
-             ### Change SQL Statements and Add check for Values
+            temp = []
+            for i in result:
+                total = {
+                    "itemName":i[0],
+                    "userID":i[1],
+                    "firstname":i[2],
+                    "lastname":i[3],
+                    "dateBorrowed":i[4],
+                    "expectedReturn":i[5]
+                }
+                temp.append(total)
+            return jsonify(temp)
+
+        elif command == "itemID" and "value" in data:
+            values = data["value"]
             cursor = db.connection.cursor()
-            cursor.execute("SELECT item_name,description,availability FROM Items;")
+            cursor.execute(f"SELECT Items.item_name,Users.userID,Users.firstname,Users.lastname,Borrow_Record.date_borrowed,Borrow_Record.expected_return_date FROM Borrow_Record INNER JOIN Users ON Borrow_Record.userID=Users.userID INNER JOIN Items ON Items.itemID=Borrow_Record.itemID WHERE Items.itemID= {values};")
             result = cursor.fetchall()
-            return jsonify({"result":"Request itemID Data"})
+            temp = []
+            for i in result:
+                total = {
+                    "itemName":i[0],
+                    "userID":i[1],
+                    "firstname":i[2],
+                    "lastname":i[3],
+                    "dateBorrowed":i[4],
+                    "expectedReturn":i[5]
+                }
+                temp.append(total)
+            return jsonify(temp)
         else:
             result = {
                 "Error": "Invalid Request"
@@ -63,16 +106,17 @@ def GetWithdrawal():
     
     else:
         result = {
-                "Error": "Invalid Request"
+                "Error": "Invalid Content-Type"
             }
         return jsonify(result),400
 
-        
+
 
 
 @app.errorhandler(404)
 def page_not_found(e):
     return "<h1>404</h1><p>The resource could not be found.</p>", 404
+
 
 
 app.run(host="0.0.0.0",port=5000)
