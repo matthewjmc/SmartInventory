ee0fce24900ce069adcba82f41ba4d5337a2e3a1
nonthicha <61011298@kmtil.ac.th>
2021-04-07 21:10:02 +0700

    UI

---
+++ UI/app.py
@@ -0,0 +1,14 @@
+from flask import (
+    Flask,
+    render_template
+)
+
+app = Flask(__name__)
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
