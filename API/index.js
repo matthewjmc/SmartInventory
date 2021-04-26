7000013e93227164762eb1fb3fbb7cbca8d5f464
mattheweis <61011279@kmitl.ac.th>
2021-04-26 23:12:29 +0700

    https for node

--- API/index.js
+++ API/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 require("dotenv").config()
+const fs = require("fs")
+
+//HTTPS Configuration
+const https = require("https")
+var privKey = fs.readFileSync("./sslcert/cert.key","utf-8")
+var certificate = fs.readFileSync("./sslcert/cert.crt","utf-8")
+var credentials = {key: privKey, cert: certificate};
 
 
 //Preconfigured Routes
@@ -16,10 +23,6 @@ app.get('/', function(req, res) {
   res.json({Invalid:"Request to API Backend"});
 });
 
-const port = process.env.PORT;
-app.listen(port , () => console.log('App listening on port ' + port));
-
-
 function clientErrorHandler (err, req, res, next) {
   if (req.xhr) {
     res.status(500).send({ error: 'Something failed!' })
@@ -27,3 +30,8 @@ function clientErrorHandler (err, req, res, next) {
     next(err)
   }
 }
+
+// const port = process.env.PORT;
+// app.listen(port , () => console.log('App listening on port ' + port));
+var httpsServer = https.createServer(credentials, app);
+httpsServer.listen(8443)
